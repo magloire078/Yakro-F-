d12085a3dadf24e41d50afe83df28385e653eb05
Magloire Kouadio <magloire078@gmail.com>
2025-09-15 12:57:17 +0000

    File changes

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -9,7 +9,7 @@ const Card = React.forwardRef<
   <div
     ref={ref}
     className={cn(
-      "rounded-lg border bg-card text-card-foreground shadow-sm",
+      "rounded-lg border-t border-l border-r border-b bg-card text-card-foreground shadow-sm",
       className
     )}
     {...props}
