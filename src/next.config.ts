e296b9b703a1487be54566c6029d6e91892436d6
Magloire Kouadio <magloire078@gmail.com>
2025-11-16 12:39:03 +0000

    ok

--- src/next.config.ts
+++ src/next.config.ts
@@ -19,12 +19,6 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
         hostname: 'firebasestorage.googleapis.com',
       },
       {
